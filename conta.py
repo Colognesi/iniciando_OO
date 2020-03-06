54820f4fb8389e3c2648f01d2d07581f67237185
Kaique Colognesi <kaique.c.oliveira@hotmail.com>
2020-03-06 16:58:09 -0300

    Metodos Privados e estaticos
    Introduzido metodos estaticos e privados na conta para melhor encapsulamento do codigo.

--- conta.py
+++ conta.py
@@ -7,13 +7,18 @@ class Conta:
         self.__limite = limite
 
     def extrato(self):
-        print("O saldo: {} do titular: {}".format(self.__saldo, self.__titular))
+        print("O saldo: {} do titular: {}".format(self.saldo, self.titular))
+
+    # Usado para validação se o valor pode ou nao ser sacado
+    def __pode_sacar(self, valor_a_sacar):
+        valor_disponivel_a_sacar = self.__saldo + self.__limite
+        return valor_a_sacar <= valor_disponivel_a_sacar
 
     def saca(self, valor):
-        if self.__saldo < valor:
-            print("Saldo insuficiente para esta operação")
-        else:
+        if self.__pode_sacar(valor):
             self.__saldo -= valor
+        else:
+            print("O valor {} passou o limite".format(valor))
 
     def deposita(self, valor):
         self.__saldo += valor
@@ -21,3 +26,30 @@ class Conta:
     def transfere(self, valor, destino):
         self.saca(valor)
         destino.deposita(valor)
+
+    # definido para representar um Get
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    @property
+    def get_titular(self):
+        return self.__titular
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    # Definido para representar um set
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
+
+    @staticmethod
+    def codigo_banco():
+        return "001"
+
+    # Metodo usado para definir este metodo sem a necessidade de iniciar o objeto
+    @staticmethod
+    def codigos_bancos():
+        return {'BB': '001', 'Caixa': '104', 'Bradesco': '237'}
